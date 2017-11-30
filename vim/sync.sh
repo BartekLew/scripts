f9b28f4062f529324df732c0b9d556fd34f3e06a
lev <barpas@zoho.com>
2017-11-30 04:48:08 +0100

    sync vimrc with git repo

--- vim/sync.sh
+++ vim/sync.sh
@@ -1,10 +1,16 @@
-#!/bin/sh -x
+#/bin/bash -x
 
 places="$HOME/.vimrc vimrc"
+localBin="$HOME/.local/bin"
+
+if [[ ! -f "$localBin/synch.sh" ]]; then
+    mkdir -p "$localBin"
+    cp sync.sh "$localBin/sync.sh"
+fi
 
 diff $places
-if [ "$?" == 1 ]; then
-    if [ `ls -t $places | tail -n1` == 'vimrc' ] ; then
+if [ "$?" == "1" ]; then
+    if [ $(ls -t $places | head -n1) == vimrc ] ; then
 		cp vimrc $HOME/.vimrc
 	else
 		cp $HOME/.vimrc vimrc
