e27309cb24bcd3bb601166fb32cdd463f7318cbc
Lew <barpas@zoho.com>
2019-06-25 11:03:59 +0200

    pipe-cl: comments

--- pipe-cl/pipe-cl.lisp
+++ pipe-cl/pipe-cl.lisp
@@ -1,17 +1,26 @@
 
 (defmacro answer (&body msg)
+	;; open output stream
 	`(with-open-file (out (second argv) :direction :output :if-exists :supersede)
-	(let ((*standard-output* out))
-	(format out "~A~%" ,@msg))))
+
+		;; redirect standard output there (in case `msg` prints)
+		(let ((*standard-output* out))
+
+			;; print result
+			(format out "~A~%" ,@msg))))
 
 (let ((argv (cdr sb-ext:*posix-argv*)))
 (if (< (length argv) 2)
 	(format t "2 parameters- pipes needed!")
-	(loop
+
+	(loop	;; If second side close stream, our get closed to
+		;; we want to reopen whenever it happens
+
 	(with-open-file (in (first argv) :direction :input)
-	(let ((*standard-input* in))
-	(loop (handler-case
-		(let ((cmd (read in nil :eof)))
-		(if (eql cmd :eof) (return)
-			(answer (eval cmd))))
-		(condition (c) (answer c)))))))))
+	(let ((*standard-input* in))			;; redirect all input to our stream
+
+	(loop (handler-case				;; handling errors
+		(let ((cmd (read in nil :eof)) )	;; read one expression from in
+		(if (eql cmd :eof) (return)		;; if eof, reopen input
+			(answer (eval cmd))))		;; else evaluate and print result
+		(condition (c) (answer c)))))))))	;; print if error happened
